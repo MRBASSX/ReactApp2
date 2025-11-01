1f1261a394a58c25dd4c105f56c09d3c75454673
mrbassx <Abassiddrisu9222@gmail.com>
2025-11-01 14:15:42 +0000

    updated...

---
+++ Exams.jsx
@@ -0,0 +1,28 @@
+Theory 
+
+
+Explain the key differences between React, Angular, and Vue in terms of architecture, data binding, and learning curve.
+
+What are Single Page Applications (SPAs)? Explain how they differ from traditional multi-page web applications.
+
+Describe the concept of state management. Why is it important in modern frontend frameworks?
+
+Discuss component-based architecture and explain how it enhances reusability and maintainability.
+
+Compare and contrast Redux and Vuex in terms of their core principles and workflow.
+
+Explain the lifecycle of a component in one of the frameworks (React, Angular, or Vue) with an example.
+
+
+
+
+Practical
+
+Create a simple SPA with React Router that has at least Five pages: Home, About, Register ,Login and Contact .
+
+Implement state management for user data using Redux or useState and useContext.
+
+Include a reusable Navbar ,Footer, Card and CoverPage component that allows navigation between pages.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+
+Ends At 12:40pm
